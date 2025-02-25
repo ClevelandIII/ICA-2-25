07df68edb8fff2c5549d6eb4596aec1f7f8101de
ClevelandIII <kingslime108@gmail.com>
2025-02-25 14:42:02 -0700

    Update

---
+++ script.js
@@ -0,0 +1,35 @@
+//first section
+document.getElementById("firstPara").innerHTML +=
+    "<span class='tan'>This is new tan text</span>";
+
+//second section
+let images = document.getElementsByTagName("img");
+
+for (let image of images) {
+    image.width = "250";
+}
+
+//third section
+let medBlue = document.getElementsByClassName("med-blue");
+let lightBlue = document.getElementsByClassName("light-blue");
+
+for (let med of medBlue) {
+    med.style.color = "#3C5E73";
+}
+
+for (let light of lightBlue) {
+    light.style.color = "#7C9EA6";
+}
+
+//fourth section
+let svg = document.querySelectorAll("#svgs svg");
+let colors = ["#283040", "#3C5E73", "#7C9EA6", "#D9BCA3", "#F2DCC9"];
+
+for (let i = 0; i < svg.length; i++) {
+    svg[i].style.stroke = colors[i];
+}
+
+//fifth section
+let span = document.getElementsByClassName("bold");
+
+span[0].style.color = "#7C9EA6";
